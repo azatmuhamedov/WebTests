cc35d25123d2ade19e2b81c4a70d74074a928735
azatmuhamedov <azotmuhamed@gmail.com>
2026-04-13 22:44:09 +0300

    создал переменные для локаторов

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -2,7 +2,18 @@ from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
-    pass
+    LOGIN_TAB = (By.XPATH, '//*[@id="login-9688310608"]')
+    QRCODE_TAB = (By.XPATH, '//*[@id="qrCode-9688310661"]')
+    LOGIN_FIELD = (By.XPATH, '//*[@id="field_email"]')
+    PASSWORD_FIELD = (By.ID, 'field_password')
+    PASSWORD_HIDDEN = (By.XPATH, '//*[@class="vkuiVisuallyHidden__host vkuiRootComponent__host"]')
+    LOGIN_BUTTON = (By.XPATH, '//*[@data-test-id="enter-action"]')
+    LOGIN_WITH_QRCODE = (By.XPATH, "//button[contains(@class,'vkuiButton__modeOutline')]")
+    CANT_SIGN_IN_BUTTON = (By.XPATH, "//button[contains(@class,'vkuiLink__host')]")
+    SIGN_UP_BUTTON = (By.XPATH, "//button[contains(@class,'vkuiButton__modeSecondary')]")
+    VK_BUTTON = (By.XPATH, '//*[@class="h-mod __small __vk_id social-icon-button"]')
+    MAIL_BUTTON = (By.XPATH, '//*[@class="i ic social-icon __s __mailru"]')
+    YA_BUTTON = (By.XPATH, '//*[@class="i ic social-icon __s __yandex"]')
 
 class LoginPageHelper(BasePage):
     pass
